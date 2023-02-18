ab183fe533abb65708607525d8ab30d2f7a429a8
Roman Borodov <robrdo@yandex.ru>
2023-02-18 23:34:32 +0100

    some stuff

--- src/businessLayer/testService.ts
+++ src/businessLayer/testService.ts
@@ -20,45 +20,84 @@ interface IRepo<T extends BaseEntity> {
     get(predicate?: Predicate<T>): T[]
     getOne(id: T['id']): Maybe<Test>
     getOne(predicate: Predicate<T>): Maybe<T>
-    update(object: Partial<T>): T
+    update(object: Partial<T>): void
     create(object: T): T
-    delete(id: T['id']): void
+    delete(object: T): void
 }
 
 export class TestRepo implements IRepo<Test> {
 
+    private readonly _dbProvider: DataProvider<Test>
+
     constructor(dbProvider?: DataProvider<Test>) {
-        this._db = dbProvider ?? new DataProvider<Test>()
+        this._dbProvider = dbProvider ?? new DataProvider<Test>()
     }
 
     get(predicate?: Predicate<Test> | undefined): Test[] {
-        return this._db.f
+        return predicate ? this._dbProvider.db.filter(predicate) : this._dbProvider.db
     }
 
     getOne(id: Number): Maybe<Test>
     getOne(predicate: Predicate<Test>): Maybe<Test>
     getOne(predicate: any): Maybe<Test> {
-        return typeof predicate === 'function' ? this._db.find(predicate) : this._db.find(x => x.id === predicate)
+        return typeof predicate === 'function' ? this._dbProvider.db.find(predicate) : this._dbProvider.db.find(x => x.id === predicate)
     }
 
-    update(object: Partial<Test>): Test {
-        throw new Error("Method not implemented.")
+    update(object: Partial<Test>): void {
+        this._dbProvider.update(object)
     }
+
     create(object: Test): Test {
-        throw new Error("Method not implemented.")
+        return this._dbProvider.add(object)
     }
-    delete(id: Number): void {
-        this._db
+    delete(object: Test): void {
+        this._dbProvider.delete(object)
     }
-
-    private readonly _db
-
 }
 
 export class DataProvider<T extends BaseEntity> {
     private readonly _db: T[] = []
-    private readonly _dbIndices: Map<T['id'], T> = []
+    private readonly _dbIndices: Map<T['id'], [T, number]> = [, [,]]
 
+    public get db() {
+        return this._db
+    }
 
-    public get db()
+    public get dbIndices() {
+        return this._dbIndices
+    }
+
+    public add(obj: T): T {
+        this._db.push(obj)
+        this._dbIndices.set(obj.id, [obj, this._db.length - 1])
+        obj.id = this.nextId
+        return obj
+    }
+
+    public update(obj: Partial<T> & { id: number }) {
+        const res = this._dbIndices.get(obj.id)?.[0]
+        if (!res) {
+            return
+        }
+        for (const [key, value] of Object.entries(obj)) {
+            res[key] = value
+        }
+    }
+
+    public delete(obj: T) {
+        const res = this._dbIndices.get(obj.id)
+        this._db.splice(res![1], 1)
+        this._dbIndices.delete(obj.id)
+    }
+
+    private _id = 1
+
+    private get nextId() {
+        return ++this._id
+    }
 }
+
+
+//today fetch some api, filter by it
+
+//TODO: implement proxy provider for array  but first implement products
